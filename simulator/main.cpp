b5c70d0468af2de8b5adfd227c17172b2b83039d
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-20 16:01:09 +0200

    Create main.cpp

---
+++ simulator/main.cpp
@@ -0,0 +1,45 @@
+#include <mbed.h>
+
+const float Vbat = 11;
+const float Imax = 1;
+
+DigitalOut myled(LED1);
+
+AnalogIn D(PA_3); // Duty Cycle
+AnalogOut V(PA_5); // Voltage
+AnalogOut I(PA_4); // Current
+
+int main() {
+  // put your setup code here, to run once:
+  float Vpv;
+  float Ipv;
+  float Iir=0.6;
+
+  while(1) {
+    myled = 0;
+    
+    // put your main code here, to run repeatedly:
+    
+    if (Iir >= 0.6) {
+          Iir = 0;
+      }
+    else {
+          Iir = Iir+0.00001;
+    } 
+    
+
+    Vpv = (1-D)*Vbat;
+
+    // calculate Current
+    if (Vpv <= 15*Iir) {
+      Ipv = Iir; // PV cell acts as current source until a certain point
+    }
+    else {
+      Ipv = (Iir/(Vbat-15*Iir))*(Vbat - Vpv); // after that (in this simulation), I decreases linearly until it reaches 0 at 11 Volts
+    }
+    
+    V=Vpv/Vbat; // write normalized Voltage
+    I=Ipv/Imax; // write normalized Current
+    
+  }
+}
