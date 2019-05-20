daa3af9e7f0ab2aa01aba6cdf89dd8ec880970fc
tomwemelsfelder <tomwemel@hotmail.com>
2019-05-20 16:01:59 +0200

    Create main.cpp

---
+++ mppt/main.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+#include "MPPT.h"
+
+/*
+Tracks the maximum power point for multiple solar arrays (solar cell groups).
+*/
+
+DigitalOut myled(LED1);
+
+// Pins for tracking group 1
+#define I1 PA_0 // Current sensor
+#define V1 PA_1 // Voltage sensor
+#define PWM1 PA_3 // Pulse width modulation output
+#define D1 PA_4 // Duty Cycle
+
+
+MPPT MPPT1(I1,V1,PWM1,D1); // Create Maximum Power Point Tracker 1
+
+//AnalogOut Duty(PA_4);
+
+int main() {
+    //Duty = 0.5;
+    while(1) {
+        myled = 1;
+
+        MPPT1.PerturbObserve(); // Execute the P&O algorithm for tracking group 1
+
+        //MPPT doesn't need to happen very fast    
+        wait_ms(10); // Track the power point every 10 ms
+    }
+}
